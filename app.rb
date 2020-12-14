6268959ea78f92026091a6969bf6009978cfb7c6
Aman Tank <amantank187@gmail.com>
2020-12-14 17:55:25 +0000

    viewing bookmark

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-require_relative 'lib/bookmark'
+require_relative './lib/bookmark'
 
 class App < Sinatra::Base
   set :session_secret, 'super secret'
@@ -8,4 +8,13 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
+
+  end
+
+  run! if app_file == $0
+
 end
